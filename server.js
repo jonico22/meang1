bd1d195a1aa993046b6520357f904b74ce44975b
Admin <admin@A51-09.local>
2016-07-08 21:52:19 -0500

    deploy node

--- server.js
+++ server.js
@@ -5,5 +5,8 @@ var path = require('path');
 app.get('/',function(req, res){
   res.sendFile(path.join(__dirname) + '/index.html');
 })
-app.listen(5000);
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 console.log('iniciando...');
